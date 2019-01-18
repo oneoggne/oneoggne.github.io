bb6d98d892128045aee0274d3f50cd2277268f75
oneoggne <oneoggne@gmail.com>
2019-01-18 22:04:04 +0500

    Test

---
+++ Test/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://lascut.ru/admin/');
+define('HTTP_CATALOG', 'http://lascut.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://lascut.ru/admin/');
+define('HTTPS_CATALOG', 'https://lascut.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/f/fr79191z/lascut.ru/public_html/admin/');
+define('DIR_SYSTEM', '/home/f/fr79191z/lascut.ru/public_html/system/');
+define('DIR_IMAGE', '/home/f/fr79191z/lascut.ru/public_html/image/');
+define('DIR_LANGUAGE', '/home/f/fr79191z/lascut.ru/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/f/fr79191z/lascut.ru/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/f/fr79191z/lascut.ru/public_html/system/config/');
+define('DIR_CACHE', '/home/f/fr79191z/lascut.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/f/fr79191z/lascut.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/f/fr79191z/lascut.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/f/fr79191z/lascut.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/f/fr79191z/lascut.ru/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/f/fr79191z/lascut.ru/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'fr79191z_main');
+define('DB_PASSWORD', '!Jinhost52');
+define('DB_DATABASE', 'fr79191z_main');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
